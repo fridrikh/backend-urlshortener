8a30d8b8cc54d60d29e234aef1cb1383c4ec1974
A.Jah <alnikf@mail.ru>
2017-03-29 12:12:25 +0300

    Configuration server, connect database (Mongo), logger, bodyparser

---
+++ src/config/development.js
@@ -0,0 +1,6 @@
+export default {
+    port: 4040,
+    mongo: {
+        mongoUri: 'mongodb://localhost/url-dev',
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import { createServer } from 'http';
 import Koa from 'koa';
+import logger from 'koa-logger';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = new Koa();
 
+mongoose.Promise = Promise;
+mongoose.connect(config.mongo.mongoUri, {
+    server: {
+        socketOptions: {
+            keepAlive: 1
+        }
+    }
+});
+
+app.use(logger());
+app.use(bodyParser());
+
 const server = createServer(app.callback());
 
-server.listen(4040, () => {
+server.listen(config.port, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(`App is running on ${host}:${port}`);
